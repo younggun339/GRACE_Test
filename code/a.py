af8df11ce7101bb92d1cd716d6c7935bb6bbcdfc
nagyeong <younggun0816@gmail.com>
2024-10-31 16:31:30 +0900

    현재 데이터 로드 과정 분석중.

--- code/a.py
+++ code/a.py
@@ -1,10 +1,54 @@
-import os
-print("현재 작업 디렉토리:", os.getcwd())
+import argparse
+from lib2to3.pytree import Base
+import torch
+import numpy as np
+import random
+import yaml
+import gc
+import time
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import average_precision_score
+from utils import load_sc_data, load_sc_causal_data, accuracy_LP
+from model import CausalGNN
+import torchmetrics
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.utils.data as Data
 
-file_path = './example/mESC/refNetwork.csv'
+# output to a file
 
-# 경로와 파일이 존재하는지 확인
-if os.path.exists(file_path):
-    print("파일이 존재합니다.")
+# set seed
+def set_rng_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+
+# input params
+parser = argparse.ArgumentParser()
+with open('param.yaml', encoding='utf-8') as f:
+    config = yaml.load(f.read(), Loader=yaml.FullLoader)['gcn']
+    for key in config.keys():
+        name = '--' + key
+        parser.add_argument(name, type=type(config[key]), default=config[key])
+args = parser.parse_args()
+
+# use cuda
+args.cuda = args.cuda and torch.cuda.is_available()
+device = torch.device("cuda" if args.cuda else "cpu")
+
+# set seed
+set_rng_seed(args.seed)
+
+# load data
+data_path = "../example/mESC/ExpressionData.csv"
+label_path = "../example/mESC/refNetwork.csv"
+
+if args.flag:
+    adj_train, feature, train_ids, val_ids, test_ids, train_labels, val_labels, test_labels = load_sc_causal_data(data_path, label_path)
 else:
-    print("파일을 찾을 수 없습니다. 경로를 확인해 주세요.")
+    adj_train, feature, train_ids, val_ids, test_ids, train_labels, val_labels, test_labels = load_sc_data(data_path, label_path)
+
+adj_train = F.normalize(adj_train, p=1, dim=1)
