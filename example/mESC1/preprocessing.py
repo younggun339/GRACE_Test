4211f7f49a77b9c58cc0e312013b162247153550
nagyeong <younggun0816@gmail.com>
2024-11-05 16:25:17 +0900

    왜 자꾸 421개.. 그것도 하필 cell 갯수와 같은 유전자만 변이 정도가 나오나했더니 scanpy의 highly_variable_gene에서는 행이 cell이고 열이 gene이어야해서 전치해야하더라...

--- example/mESC1/preprocessing.py
+++ example/mESC1/preprocessing.py
@@ -10,6 +10,7 @@ data = pd.read_csv(file_path, sep='\t', index_col=0)  # 파일 형식에 따라 
 # 숫자형 데이터와 문자열 데이터 분리
 numeric_data = data.drop(columns=['transcript_id']).values.astype(np.float64)  # 숫자 데이터만 추출
 transcript_ids = data['transcript_id'].values  # 문자열 데이터 추출
+numeric_data = numeric_data.T
 
 
 adata = sc.AnnData(X=numeric_data)
@@ -33,23 +34,13 @@ adata_TPM = sc.pp.log1p(adata, copy=True)
 
 # 3. 변동성 높은 유전자 선택
 # 변동성이 큰 상위 유전자 선택, Bonferroni 보정을 적용하여 p-값 기준 선택
-sc.pp.highly_variable_genes(adata, n_top_genes=1000, flavor='seurat_v3', batch_key=None)
+sc.pp.highly_variable_genes(adata_CPM, n_top_genes=1000, flavor='seurat_v3', batch_key=None)
+sc.pp.highly_variable_genes(adata_TPM, n_top_genes=1000, flavor='seurat_v3', batch_key=None)
 
 
 # 선택된 변동성 높은 유전자와 그들의 p-value 추출
 hv_genes = adata.var[adata.var['highly_variable']]
-p_values = hv_genes['pvals']  # pvals 컬럼은 seurat_v3 flavor에 의해 추가됨
 
-# Bonferroni 보정 적용
-_, adjusted_pvalues, _, _ = multipletests(p_values, alpha=0.01, method='bonferroni')
-
-# 보정된 p-value 기준으로 유의미한 유전자 선택
-hv_genes['adjusted_pvals'] = adjusted_pvalues
-significant_genes = hv_genes[hv_genes['adjusted_pvals'] < 0.01]
-
-# 상위 500, 1000개의 유전자 선택
-top_500_genes = significant_genes[:500]
-top_1000_genes = significant_genes[:1000]
 
 # 필터링된 데이터(유전자와 세포 모두 선택된) DataFrame으로 변환 후 CSV로 저장
 filtered_data = pd.DataFrame(adata.X, index=adata.obs_names, columns=adata.var_names)
